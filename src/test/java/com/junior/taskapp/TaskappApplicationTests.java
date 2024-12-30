c86224b3ab4199e38c506c977df5e9ffa3d692d3
juniorapeles <elijunior.apeles2005@gmail.com>
2024-12-30 20:31:52 -0300

    feat project created

---
+++ src/test/java/com/junior/taskapp/TaskappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junior.taskapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
